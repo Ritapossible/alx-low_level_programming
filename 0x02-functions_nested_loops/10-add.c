97b8d14ab5da34e8a483dd7fc6b5d8d2e1c129c7
Ritapossible <egwuaturita95@gmail.com>
2023-07-18 12:05:59 -0700

    Addition updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * File: 10-add.c
+ * Auth: Augustine Rita
+ */
+
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num11: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num11)
+{
+	return (num1 + num11);
+}
