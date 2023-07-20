4fe1359ea6e53dbc7542e5cbf79d30b07c2bf6af
Ritapossible <egwuaturita95@gmail.com>
2023-07-20 13:07:47 -0700

    print line updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Augustine Rita
+ */
+
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
