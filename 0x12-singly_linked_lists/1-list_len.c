d4abaedff834625e15292a2ec07a04fc1f34d297
Ritapossible <egwuaturita95@gmail.com>
2023-08-24 15:20:25 -0700

    function updated

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+size_t count1 = 0;
+while (h != NULL)
+
+{
+count1++;
+h = h->next;
+}
+return (count1);
+}
