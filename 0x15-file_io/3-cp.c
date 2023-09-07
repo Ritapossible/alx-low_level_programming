da61d851f0ba4e02e4c74d353996a3b815301637
Ritapossible <egwuaturita95@gmail.com>
2023-09-07 00:46:41 -0700

    file updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,6 +34,7 @@ void error_file(int file_from, int file_to, char *argv[])
  * @argv: Arguments vector.
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, error_close;
@@ -47,7 +48,7 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2],O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
 	ichars = 1024;
@@ -64,14 +65,14 @@ int main(int argc, char *argv[])
 	error_close = close(file_from);
 	if (error_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file-descriptor %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
 	error_close = close(file_to);
 	if (error_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file-descriptor %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
